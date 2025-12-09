e377dea4aa25d8f1cd9c807e198b7808b344d4eb
Mira Marta <miramarta14@gmail.com>
2025-12-09 14:04:53 +0700

    Create create_table.sql

---
+++ sql/create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE shopping_trends (
+    customer_id VARCHAR(50),
+    age INT,
+    gender VARCHAR(10),
+    category VARCHAR(100),
+    total_spent DECIMAL(10, 2),
+    purchase_date DATE,
+    payment_method VARCHAR(50),
+    city VARCHAR(50),
+    rating INT
+);
